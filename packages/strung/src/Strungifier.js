046720254ce62ea8fb23017f27548b4a4c4f17ae
Matt Garber <mgarber93@gmail.com>
2018-07-25 22:03:53 -0700

    Add style rules

--- packages/strung/src/Strungifier.js
+++ packages/strung/src/Strungifier.js
@@ -1,21 +1,21 @@
 
-function findStrings(file) {
+function findStrings (file) {
   let prevWasBackSlash = false
   let isInString = false
   let index = -1
-  let run = '';
+  let run = ''
   const strings = []
 
-  while( ++index < file.length ) {
-    if ( prevWasBackSlash ) {
-      prevWasBackSlash = false;
-    } else if ( file[index] === '"' ) {
-      if ( isInString ) {
+  while (++index < file.length) {
+    if (prevWasBackSlash) {
+      prevWasBackSlash = false
+    } else if (file[index] === '"') {
+      if (isInString) {
         strings.push(run)
         run = ''
       }
       isInString = !isInString
-    } else if ( isInString ) {
+    } else if (isInString) {
       run += file[index]
     }
   }
@@ -24,7 +24,7 @@ function findStrings(file) {
 }
 
 class Node {
-  constructor(char, freq) {
+  constructor (char, freq) {
     this.char = char
     this.freq = freq
     this.left = null
@@ -38,17 +38,17 @@ class HuffMannEncoding {
       .reduce((acc, char) => {
         acc[char] = (acc[char] || 0) + 1
         return acc
-      }, {});
+      }, {})
   }
 }
 
 class Strungifier {
-  constructor(options = {}) {
+  constructor (options = {}) {
     this.strings = []
     Object.assign(this, options)
   }
 
-  strungify(file) {
+  strungify (file) {
     const strings = findStrings(file)
     return file
   }
