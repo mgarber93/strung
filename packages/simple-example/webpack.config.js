c9858c29b5ea5630a7f2d01723b097f8db176e0f
Matt Garber <mgarber93@gmail.com>
2018-08-05 19:02:52 -0700

    Remove comments

--- packages/simple-example/webpack.config.js
+++ packages/simple-example/webpack.config.js
@@ -2,14 +2,11 @@ const path = require('path')
 const Strung = require('strung')
 
 module.exports = {
-  mode: 'development', // "production" | "development" | "none"  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // string
-    filename: 'bundle.js', // string    // the filename template for entry chunks
-    publicPath: '/assets/', // string    // the url to the output directory resolved relative to the HTML page
-    library: 'MyLibrary', // string,
-    libraryTarget: 'umd'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   target: 'node',
   stats: 'errors-only',
