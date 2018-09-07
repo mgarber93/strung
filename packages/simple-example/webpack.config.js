188f6723a6c913cdf834d6b7e03b74d77b34cd2a
Matt Garber <mgarber93@gmail.com>
2018-09-06 21:29:10 -0700

    Emit bundle and minified bundle

--- packages/simple-example/webpack.config.js
+++ packages/simple-example/webpack.config.js
@@ -1,16 +1,28 @@
 const path = require('path')
 const Strung = require('strung')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    target: 'node',
+    stats: 'errors-only',
   },
-  target: 'node',
-  stats: 'errors-only',
-  plugins: [
-    new Strung({})
-  ]
-}
+  {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.min.js',
+    },
+    target: 'node',
+    stats: 'errors-only',
+    plugins: [
+      new Strung({}),
+    ]
+  },
+]
